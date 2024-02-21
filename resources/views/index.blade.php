dabfd284492e4226ea684eb59c30d3b1ac0e6b4c
samu.sar <samuil.sarandev@gmail.com>
2024-02-21 15:40:35 +0200

    Add: added initial single task show view adn route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <div>
         @forelse($tasks as $task)
             <div>
-                {{$task->title}}
+                <a href="{{ route('tasks.show', ['id' => $task->id])}}">{{ $task->title }}</a>
             </div>
         @empty
             <div>There are no tasks!</div>
