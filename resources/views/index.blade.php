c559cb976708415a337459704eba28dd9c4fb3d2
samu.sar <samuil.sarandev@gmail.com>
2024-02-22 19:04:14 +0200

    Add: added task toggle-complete functionality and route and edit / create buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Task List')
 
 @section('content')
+    <div>
+        <a href="{{ route('tasks.create') }}">Add Task</a>
+    </div>
+
     @forelse($tasks as $task)
         <div>
             <a href="{{ route('tasks.show', ['task' => $task->id])}}">{{ $task->title }}</a>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,8 +12,28 @@
 <p>{{$task->created_at}}</p>
 <p>{{$task->updated_at}}</p>
 
+<p>
+    @if ($task->completed)
+        Completed
+    @else
+        Not completed
+    @endif
+</p>
+
 <div>
-    <form method="POST" action="{{ route('tasks.destroy', ['task' => $task->id]) }}">
+    <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+</div>
+
+<div>
+    <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
+        @csrf
+        @method('PUT')
+        <button type="submit">Mark as {{ $task->completed ? 'not completed' : 'completed' }}</button>
+    </form>
+</div>
+
+<div>
+    <form method="POST" action="{{ route('tasks.destroy', ['task' => $task]) }}">
         @csrf
         @method('DELETE')
         <button type="submit">Delete</button>
