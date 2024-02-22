38432bd5eddb0b9394fecb263a6a8005c51958da
samu.sar <samuil.sarandev@gmail.com>
2024-02-22 20:12:29 +0200

    Style: setup tailwindcss and style task index and show views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,25 @@
 <html>
     <head>
         <title>Task List Demo App</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        {{-- blade-formatter-disable --}}
+        <style type="text/tailwindcss">
+            .btn {
+                @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 text-slate-700 hover:bg-slate-50
+            }
+
+            .link {
+                @apply font-medium text-gray-700 underline decoration-pink-500
+            }
+        </style>
+        {{-- blade-formatter-enable --}}
+
         @yield('styles')
     </head>
 
-    <body>
-        <h1>
+    <body class="container mx-auto mt-10 mb-10 max-w-lg">
+        <h1 class="text-2xl mb-4">
             @yield('title')
         </h1>
         <div>
