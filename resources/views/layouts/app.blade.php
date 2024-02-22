492705a36ab74c064bf3ee89855e244a83e7e4b5
samu.sar <samuil.sarandev@gmail.com>
2024-02-22 20:41:49 +0200

    Style: styled forms and added flash message reactive close with alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <title>Task List Demo App</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="//unpkg.com/alpinejs" defer></script>
 
         {{-- blade-formatter-disable --}}
         <style type="text/tailwindcss">
@@ -12,6 +13,18 @@
 
             .link {
                 @apply font-medium text-gray-700 underline decoration-pink-500
+            }
+
+            label {
+                @apply block uppercase text-slate-700 mb-2
+            }
+
+            input, textarea {
+                @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight rounded-sm focus:outline-none
+            }
+
+            .error {
+                @apply text-red-500 text-sm
             }
         </style>
         {{-- blade-formatter-enable --}}
@@ -23,10 +36,25 @@
         <h1 class="text-2xl mb-4">
             @yield('title')
         </h1>
-        <div>
+        <div x-data="{ flash: true }">
             @if (session()->has('success'))
-                <div>{{ session('success') }}</div>
+                <div
+                    x-show="flash"
+                    class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700"
+                    role="alert">
+                    <strong class="font-bold">Success!</strong>
+                    <div>{{ session('success') }}</div>
+
+                    <span @click="flash = false" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" @click="flash = false"
+                            stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </span>
+                </div>
             @endif
+
             @yield('content')
         </div>
     </body>
